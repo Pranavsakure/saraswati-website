35fe28ee7688b687048c6046a2d4ba46da0c0b8a
Pranavsakure <105531423+Pranavsakure@users.noreply.github.com>
2022-09-05 21:42:50 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 var app = express()
 const fs = require("fs")
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = express.Router();
 
 // app.METHOD(PATH,HANDLER)
