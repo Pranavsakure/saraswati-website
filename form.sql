32eabf3006bc781fd60bb8ccf6cdea4ac00f483e
Pranavsakure <105531423+Pranavsakure@users.noreply.github.com>
2022-09-08 21:50:21 +0530

    local database complete

---
+++ form.sql
@@ -0,0 +1,5 @@
+CREATE TABLE query(
+roll_no INT NOT NULL,
+room_no INT NOT NULL,
+full_name VARCHAR NOT NULL,
+Problems VARCHAR NOT NULL);
--- index.js
+++ index.js
@@ -1,26 +1,69 @@
 const express = require('express')
 const path = require('path');
 var app = express()
-const fs = require("fs")
+const fs = require("fs");
+const bodyparser = require('body-parser'); 
 const port = process.env.PORT || 3000;
 const router = express.Router();
 
-// app.METHOD(PATH,HANDLER)
+const {Client} = require('pg');
+const { ClientRequest } = require('http');
+const client = new Client({
+    host:"localhost",
+    user:"postgres",
+    port:"5432",
+    password:"12345678",
+    database:"test"
+})
+// async function main() {
+//     await client.connect();  
+// }
+client.connect();
 
-// app.set('html',path.join(__dirname,'cauvery-website-main')); 
 
-// app.get("/",(req,res)=>{ 
-//     // res.set('Content-Type', 'text/html')
-//     res.status(200).render(path.join(__dirname+'index.html'))
-// });
+client.query(`SELECT * from idea`,(err,res)=>{
+    if(!err){
+        console.log(res.rows);
+    }else{
+        console.log(err.message);
+    }
+    client.end;
+})
+
+
+
+
+
+
+
 app.use(express.static('static'));
+app.use(express.urlencoded())
+// app.use(express.json());
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
-  });
+});
 
 app.use('/',router);
 
+app.post('/problems',(req,res)=>{
+    client.query(
+    `INSERT into query(no_roll,full_name,room_no,problems
+    ) VALUES(
+        '${req.body.roll_num}',
+        '${req.body.name}',
+        '${req.body.room_num}',
+        '${req.body.problem}');`
+    )
+    res.end('Added Complain');
+});
+app.post('/suggestions',(req,res)=>{
+    client.query(
+    `INSERT into idea(suggest
+    ) VALUES('${req.body.suggestion}');`
+    )
+    res.end('Added suggestions');
+})
 app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
 });
